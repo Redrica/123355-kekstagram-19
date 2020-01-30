ed3e3900fa710fd652ddf139fd462d634617d53c
Redrica <de.spring@gmail.com>
2020-01-30 20:11:28 +0300

    Разделены перечисления, убрана генерация случайных номеров фотографий, исправлены названия переменных, подкорректированы функции.

--- js/main.js
+++ js/main.js
@@ -8,61 +8,40 @@ var Picture = {
   DESCRIPTION: 'Описание вот этой конкретной фотографии – ',
   LIKES_MIN: 15,
   LIKES_MAX: 200,
-  Comment: {
-    AVATAR_URL_SUBSTRING: 'img/avatar-',
-    AVATAR_EXTENSION: '.svg',
-    AVATAR_NUMBER_MIN: 1,
-    AVATAR_NUMBER_MAX: 6,
-    MESSAGES: [
-      'Всё отлично!',
-      'В целом всё неплохо. Но не всё.',
-      'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-      'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-      'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-      'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-    ],
-    MIN_MESSAGE_STRINGS: 1,
-    MAX_MESSAGE_STRINGS: 2,
-    NAMES: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
-    COMMENTS_QUANTITY_MIN: 0,
-    COMMENTS_QUANTITY_MAX: 10,
-  }
 };
+// Eslint: Comment is already defined as built-in (…). Не нашла Comment в списке зарезервированных слов Оо, но низя так низя.
+var UserComment = {
+  AVATAR_URL_SUBSTRING: 'img/avatar-',
+  AVATAR_EXTENSION: '.svg',
+  AVATAR_NUMBER_MIN: 1,
+  AVATAR_NUMBER_MAX: 6,
+  MIN_MESSAGE_STRINGS: 1,
+  MAX_MESSAGE_STRINGS: 2,
+  NAMES: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
+  COMMENTS_QUANTITY_MIN: 0,
+  COMMENTS_QUANTITY_MAX: 10,
+};
+var MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
 
 // получение случайного целого числа от min до max
-var getRandomNumber = function (min, max) {
+var getRandomNumber = function (max, min) {
   var minNumber = min ? min : 0;
-  return Math.floor(Math.random() * (max - minNumber + 1)) + minNumber;
-};
-
-// создание массива последовательных значений целых чисел от min до max
-var makeNumericArray = function (min, max) {
-  var numericArray = [];
-  for (var i = min; i <= max; i++) {
-    numericArray.push(i);
-  }
-  return numericArray;
-};
-
-// перемешивание массива
-var shuffleArray = function (array) {
-  var j;
-  var temp;
-  for (var i = array.length - 1; i > 0; i--) {
-    j = Math.floor(Math.random() * (i + 1));
-    temp = array[j];
-    array[j] = array[i];
-    array[i] = temp;
-  }
-  return array;
+  return Math.round(Math.random() * (max - minNumber)) + minNumber;
 };
 
 // генерация текста сообщения из случайного количества строк по массиву строк
 var generateCommentMessage = function () {
-  var stringsQuantity = getRandomNumber(Picture.Comment.MIN_MESSAGE_STRINGS, Picture.Comment.MAX_MESSAGE_STRINGS);
+  var stringsQuantity = getRandomNumber(UserComment.MIN_MESSAGE_STRINGS, UserComment.MAX_MESSAGE_STRINGS);
   var message = '';
   for (var i = 0; i < stringsQuantity; i++) {
-    message += Picture.Comment.MESSAGES[getRandomNumber(false, (Picture.Comment.MESSAGES.length - 1))] + ' ';
+    message += MESSAGES[getRandomNumber(false, (MESSAGES.length - 1))] + ' ';
   }
   return message;
 };
@@ -70,15 +49,15 @@ var generateCommentMessage = function () {
 // генерация объекта комментария
 var generateComment = function () {
   return {
-    avatar: Picture.Comment.AVATAR_URL_SUBSTRING + getRandomNumber(Picture.Comment.AVATAR_NUMBER_MIN, Picture.Comment.AVATAR_NUMBER_MAX) + Picture.Comment.AVATAR_EXTENSION,
+    avatar: UserComment.AVATAR_URL_SUBSTRING + getRandomNumber(UserComment.AVATAR_NUMBER_MIN, UserComment.AVATAR_NUMBER_MAX) + UserComment.AVATAR_EXTENSION,
     message: generateCommentMessage(),
-    name: Picture.Comment.NAMES[getRandomNumber(false, (Picture.Comment.NAMES.length - 1))],
+    name: UserComment.NAMES[getRandomNumber((UserComment.NAMES.length - 1))],
   };
 };
 
 // сбор массива комментариев для одного фото
 var makePictureCommentsArray = function () {
-  var commentsQuantity = getRandomNumber(Picture.Comment.COMMENTS_QUANTITY_MIN, Picture.Comment.COMMENTS_QUANTITY_MAX);
+  var commentsQuantity = getRandomNumber(UserComment.COMMENTS_QUANTITY_MIN, UserComment.COMMENTS_QUANTITY_MAX);
   var pictureComments = [];
   for (var i = 0; i < commentsQuantity; i++) {
     pictureComments.push(generateComment());
@@ -87,43 +66,40 @@ var makePictureCommentsArray = function () {
 };
 
 // создание массива c объектами фотографий фотографий
-var generatePictures = function (quantity, numbers) {
-  var picturesObjectsArray = [];
+var generatePictures = function (quantity) {
+  var pictures = [];
   for (var i = 0; i < quantity; i++) {
     var picture = {
-      url: Picture.PICTURE_URL_SUBSTRING + numbers[i] + Picture.PICTURE_EXTENSION,
-      description: Picture.DESCRIPTION + numbers[i] + Picture.PICTURE_EXTENSION,
+      url: Picture.PICTURE_URL_SUBSTRING + (i + 1) + Picture.PICTURE_EXTENSION,
+      description: Picture.DESCRIPTION + (i + 1) + Picture.PICTURE_EXTENSION,
       likes: getRandomNumber(Picture.LIKES_MIN, Picture.LIKES_MAX),
       comments: makePictureCommentsArray(),
     };
-    picturesObjectsArray.push(picture);
+    pictures.push(picture);
   }
-  return picturesObjectsArray;
+  return pictures;
 };
 
 // создаем сущность фотографии по объекту
-var createPictureElement = function (pictureObject) {
+var createPictureElement = function (picture) {
   var pictureElement = document.querySelector('#picture').content.querySelector('.picture').cloneNode(true);
-  pictureElement.querySelector('.picture__img').src = pictureObject.url;
-  pictureElement.querySelector('.picture__likes').textContent = pictureObject.likes;
-  pictureElement.querySelector('.picture__comments').textContent = pictureObject.comments.length + '';
+  pictureElement.querySelector('.picture__img').src = picture.url;
+  pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+  pictureElement.querySelector('.picture__comments').textContent = picture.comments.length + '';
   return pictureElement;
 };
 
 // отрисовка фотографий
-var renderPictures = function (picturesObjectsArray) {
+var renderPictures = function (pictures) {
   var picturesFragment = document.createDocumentFragment();
-  for (var i = 0; i < picturesObjectsArray.length; i++) {
-    picturesFragment.appendChild(createPictureElement(picturesObjectsArray[i]));
+  for (var i = 0; i < pictures.length; i++) {
+    picturesFragment.appendChild(createPictureElement(pictures[i]));
   }
   return picturesFragment;
 };
 
-// массив с уникальными номерами фотографий
-var pictureNumbers = shuffleArray(makeNumericArray(Picture.PICTURE_NUMBER_MIN, Picture.PICTURE_NUMBER_MAX));
-
 // итоговый массив с фотографиями для отрисовки
-var pictures = generatePictures(Picture.PICTURE_NUMBER_MAX, pictureNumbers);
+var pictures = generatePictures(Picture.PICTURE_NUMBER_MAX);
 
 // отрисовка в DOM
 var picturesPlace = document.querySelector('.pictures');
